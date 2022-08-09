77a2eafef44575f1b236b13f69fad154453bed94
Hyun Woo <hwsa10041@gmail.com>
2022-08-09 21:41:17 +0900

    의존성 주입 및 세팅 테스트
    정상 작동 확인

---
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER/HKurlyServerApplication.java
@@ -0,0 +1,13 @@
+package dongyang.one.hackathon.H_KURLY_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HKurlyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HKurlyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dongyang/one/hackathon/H_KURLY_SERVER/HKurlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dongyang.one.hackathon.H_KURLY_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HKurlyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
